d590b99acf1bef25166604ff7a748445b1999513
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-10-24 12:54:52 +0800

    git use monolog Basic Usage

--- public/index.php
+++ public/index.php
@@ -5,14 +5,27 @@ require '../vendor/autoload.php';
 //initiative import namespace
 use Demo\Hello\Lara;
 use Demo\Hello\Someone;
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 $lara = new Lara();
 $vincent = new Someone('Vincent');
 
 //passive import namespace
-$mary = new \Demo\Hello\Someone('Mary');
+$mary = new Demo\Hello\Someone('Mary');
 $john = new Demo\Hello\Someone('John');
 $hello = new Demo\HelloWorld();
 
 //類別別名
 use Demo\HelloWorld as World;
 $world = new World();
+
+// create a log channel
+$log = new Logger('WISD');
+$log->pushHandler(new StreamHandler('path/to/log/my.log', Level::Warning));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
